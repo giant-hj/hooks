5583868f99f4755ba2229339129dd83a1f3c5f5b
Heonjong Lee <heonjonglee@Heonjongui-MacBookPro.local>
2020-06-26 17:48:21 +0900

    #1.2 useInput part Two

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import useInput from "./useInput";
 
 import "./styles.css";
 
-const useInput = initialValue => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = event => {
-    console.log(event.target);
-  }
-  return { value, onChange };
-};
-
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLangth = value => !value.includes("@");
+  const name = useInput("Mr.", maxLangth);
   return (
     <div className="App">
       <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
+      <input placeholder="Name" {...name.props} />
     </div>
   );
 };
---
+++ src/useInput.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = event => {
+      const {
+        target: { value }
+      } = event;
+      let willUpdate = true;
+      if (typeof validator === "function") {
+        willUpdate = validator(value);
+      }
+      if (willUpdate) {
+        setValue(value);
+      }
+    };
+    return { props: { value, onChange }, utils: { setValue } };
+  };
+
+  export default useInput;
