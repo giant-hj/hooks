7fbda47ad203e68b867a64a86b5d2050eddbd457
Heonjong Lee <heonjonglee@Heonjongui-MacBookPro.local>
2020-06-26 15:40:22 +0900

    #1.1 useInput

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
+import "./styles.css";
+
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    console.log(event.target);
+  }
+  return { value, onChange };
+};
+
+const App = () => {
+  const name = useInput("Mr.");
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
