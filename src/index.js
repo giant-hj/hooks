ad930c01f172a3ba80457a8ed3b50db229f5c42d
Heonjong Lee <heonjonglee@Heonjongui-MacBookPro.local>
2020-06-27 11:00:11 +0900

    #2.0 useEffect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import useInput from "./useInput";
+import useTabs from "./useTabs";
 
 import "./styles.css";
 
+
+const App = () => {
+  const sayHello = () => { console.log("hello")};
+  const [number, setNumber] = useState(0);
+  const [aNumber, setANumber] = useState(0);
+  useEffect(sayHello, [number]);
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  )
+}
+
+/*
+const contents = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+]
+
+const App = () => {
+  const { currentItem, changeItem } = useTabs(0, contents);
+  return (
+    <div className="App">
+      {contents.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
+}*/
+
+/*
 const App = () => {
   const maxLangth = value => !value.includes("@");
   const name = useInput("Mr.", maxLangth);
@@ -14,6 +55,7 @@ const App = () => {
     </div>
   );
 };
+*/
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
