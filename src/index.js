3586535f1e160d22ae59a152f98bf15152a9d541
Heonjong Lee <heonjonglee@Heonjongui-MacBookPro.local>
2020-06-29 15:50:12 +0900

    #2.1 useTitle

--- src/index.js
+++ src/index.js
@@ -5,20 +5,41 @@ import useTabs from "./useTabs";
 
 import "./styles.css";
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  }
+  useEffect(updateTitle, [title]);
+  return setTitle;
+}
 
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  //setTimeout(() => titleUpdater("Home"), 5000);
+  return (
+    <div className="App">
+      <div>Hi</div>
+    </div>
+  )
+}
+
+/*
 const App = () => {
   const sayHello = () => { console.log("hello")};
   const [number, setNumber] = useState(0);
   const [aNumber, setANumber] = useState(0);
-  useEffect(sayHello, [number]);
+  useEffect(sayHello, [number]);  //componentDidMount, componentWillUpdate 
   return (
     <div className="App">
-      <div>Hi</div>
+      <div>Hi</div> 
       <button onClick={() => setNumber(number + 1)}>{number}</button>
       <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
     </div>
   )
-}
+}\
+*/
 
 /*
 const contents = [
