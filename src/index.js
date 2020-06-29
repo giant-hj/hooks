ea171f8a0d06ce49fb3c4cebab70bfc19d79a179
Heonjong Lee <heonjonglee@Heonjongui-MacBookPro.local>
2020-06-29 17:06:47 +0900

    #2.1 useTitle

--- src/index.js
+++ src/index.js
@@ -2,18 +2,9 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import useInput from "./useInput";
 import useTabs from "./useTabs";
+import useTitle from "./useTitle";
 
 import "./styles.css";
-
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  }
-  useEffect(updateTitle, [title]);
-  return setTitle;
-}
 
 const App = () => {
   const titleUpdater = useTitle("Loading...");
